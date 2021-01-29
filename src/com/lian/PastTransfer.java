bb393d4e0bacec4b367b45a07c5dfb44e9080433
lyc <yuechuan.lian@joymo.tech>
2021-01-29 15:29:18 +0800

    3.0

--- src/com/lian/PastTransfer.java
+++ src/com/lian/PastTransfer.java
@@ -10,42 +10,41 @@ import com.intellij.openapi.editor.Caret;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.SelectionModel;
-import com.intellij.openapi.ide.CopyPasteManager;
 import com.intellij.openapi.project.Project;
-import javafx.scene.input.Clipboard;
-import org.apache.commons.lang.ArrayUtils;
-import org.apache.http.util.TextUtils;
-import org.codehaus.groovy.runtime.ArrayUtil;
 
-import java.awt.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 
-public class AsTranslate extends AnAction {
+public class PastTransfer extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
         final Project project = e.getRequiredData(CommonDataKeys.PROJECT);
         final Editor mEditor = e.getData(PlatformDataKeys.EDITOR);
-        if (null == mEditor) {
+        String textInClipboard = ClipboardUtil.getTextInClipboard();
+        if (null == textInClipboard || "".equals(textInClipboard)) {
             return;
         }
         SelectionModel model = mEditor.getSelectionModel();
         final String selectedText = model.getSelectedText();
-        if (TextUtils.isEmpty(selectedText)) {
-            return;
-        }
+
         final Document document = mEditor.getDocument();
         // Work off of the primary caret to get the selection info
         Caret primaryCaret = mEditor.getCaretModel().getPrimaryCaret();
         int start = primaryCaret.getSelectionStart();
         int end = primaryCaret.getSelectionEnd();
-        String[] split = selectedText.split("\n");
+        String[] split1 = textInClipboard.split("\n");
+        ArrayList<String> strings = new ArrayList<>();
+        for (String s : split1) {
+
+            String s1 = s.split("`")[1];
+            strings.add(s1);
+        }
+//        selectedText.split("\n");
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < split.length; i++) {
-            String s = upperTable(split[i].trim());
-            String ss = split[i] + " " + "AS" + " " + s;
-            stringBuilder.append(ss).append(i == (split.length - 1) ? "" : ",\n");
+        for (int i = 0; i < strings.size(); i++) {
+            String s = upperTable(strings.get(i).trim());
+            String ss = strings.get(i) + " " + "AS" + " " + s;
+            stringBuilder.append(ss).append(i == (strings.size() - 1) ? "" : ",\n");
         }
         WriteCommandAction.runWriteCommandAction(project, () ->
                 document.replaceString(start, end, stringBuilder)
@@ -58,26 +57,26 @@ public class AsTranslate extends AnAction {
     public static String upperTable(String str) {
         // 字符串缓冲区
         StringBuffer sbf = new StringBuffer();
-            // 按下划线来切割字符串为数组
-            String[] split = str.split("_");
-            if (split.length == 1) {
-                sbf.append(split[0]);
-                return sbf.toString();
-            }
-            // 循环数组操作其中的字符串
-            for (int i = 0, index = split.length; i < index; i++) {
-                if (i == 0) {
-                    sbf.append(split[i]);
-                    continue;
-                }
-                // 递归调用本方法
-                String upperTable = toBig(split[i]);
-                // 添加到字符串缓冲区
-                sbf.append(upperTable);
-            }
-            // 返回
+        // 按下划线来切割字符串为数组
+        String[] split = str.split("_");
+        if (split.length == 1) {
+            sbf.append(split[0]);
             return sbf.toString();
         }
+        // 循环数组操作其中的字符串
+        for (int i = 0, index = split.length; i < index; i++) {
+            if (i == 0) {
+                sbf.append(split[i]);
+                continue;
+            }
+            // 递归调用本方法
+            String upperTable = toBig(split[i]);
+            // 添加到字符串缓冲区
+            sbf.append(upperTable);
+        }
+        // 返回
+        return sbf.toString();
+    }
 
     public static String toBig(String str) {
         // 转换成字符数组
